dea195b3536eba8be4573e6a0bfdd10f75bf6b1b
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-28 10:39:34 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "simple-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  progress = 20;
+export class AppComponent implements OnInit {
+  progress = 0;
+  ngOnInit(): void {
+    this.updateProgress(6);
+  }
+  updateProgress(value: number): void {
+    if (this.progress >= 100 || value === 0) {
+      return;
+    }
+    const delta = 100 - this.progress;
+    if (value > delta) {
+      value = delta;
+    }
+    setTimeout(() => {
+      this.progress += value;
+      this.updateProgress(value);
+    }, 200);
+  }
 }
