79429d814c40eed1a0abe7b09441a48a0975b251
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-28 10:50:47 +0100

    3 files modified

--- src/app/3ml.progress-bar/3ml.comp.progress-bar.ts
+++ src/app/3ml.progress-bar/3ml.comp.progress-bar.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
 @Component({
   selector: "simple-progress-bar",
   template: `
     <div class="container">
       <div class="progress" [style.width.%]="progress">{{ progress }}%</div>
     </div>
+    <button (click)="onClick($event)">+{{ increment }}</button>
   `,
   styles: [
     `
@@ -23,9 +25,40 @@ import { Component, Input } from "@angular/core";
         border-radius: inherit;
         transition: width 0.5s ease-out;
       }
+      button {
+        color: white;
+        background-color: darkseagreen;
+        border: none;
+        border-radius: 2em;
+        padding: 1em 2em;
+        font-size: 0.8em;
+        transform: scale(1);
+        outline: none;
+        cursor: pointer;
+      }
+      button:focus {
+        background-color: rgb(108, 161, 108);
+        transform: scale(1.05);
+        box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.26);
+      }
+      button:active {
+        background-color: rgb(100, 148, 100);
+        transform: scale(0.8);
+        box-shadow: none;
+      }
     `
   ]
 })
 export class ProgressBar {
   @Input() progress: number;
+  @Output() progressChange = new EventEmitter<number>();
+  private increment = 15;
+  onClick(event: MouseEvent) {
+    const delta = 100 - this.progress;
+    if (delta > this.increment) {
+      this.progressChange.emit(this.increment);
+    } else {
+      this.progressChange.emit(delta);
+    }
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,8 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AppComponent implements OnInit {
   progress = 0;
-  ngOnInit(): void {
-    this.updateProgress(6);
-  }
+  ngOnInit(): void {}
   updateProgress(value: number): void {
-    if (this.progress >= 100 || value === 0) {
-      return;
-    }
-    const delta = 100 - this.progress;
-    if (value > delta) {
-      value = delta;
-    }
-    setTimeout(() => {
-      this.progress += value;
-      this.updateProgress(value);
-    }, 200);
+    this.progress += value;
   }
 }
