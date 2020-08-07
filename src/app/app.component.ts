ddbb2172457f0a8cf4b824c15f39150c70d3bc08
MatteoMit <matteo.mitrano@3mlinformatica.it>
2020-08-07 11:33:48 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "simple-root",
   template: `
+    <h3> {{title}} </h3>
     <simple-counter1
       title="Componente 1"
-      [counterSimple1]="counter1"
+      [(counterSimple1)] = "counter1">
       (update1)="updateCounter1($event)"
     ></simple-counter1>
-    <br>
-    <p> ------ ------ ------ ------ ----- </p>
+    <br />
+    <p>------ ------ ------ ------ -----</p>
     <simple-counter2
       title="Componente 2"
       [counterSimple2]="counter2"
@@ -17,15 +18,28 @@ import { Component } from "@angular/core";
   `
 })
 export class AppComponent {
-
-  counter1 = 0;
+  title = "Scambio di informazioni fra componenti adiacenti";
+  private _counter1 = 0;
   counter2 = 0;
 
-  updateCounter1 (value: number){
+  /* 
+  --OLD - BUT working! - version for "updadateCounter1" method  
+  --
+  updadateCounter1 (value: number){
     this.counter2 += value;
   }
-  updateCounter2 (value: number){
-    this.counter1 += value;
+*/
+  // NEW versione for COUNTER1
+  get counter1(): number {
+    console.log("get counter, counter1: " + this._counter1);
+    return this._counter1;
+  }
+  set counter1(value: number) {
+    this.counter2 += value;
+    console.log("set counter, counter2: " + this.counter2);
   }
 
+  updateCounter2(value: number) {
+    this.counter1 += value;
+  }
 }
--- src/app/simple-counter1/simple-counter1.component.ts
+++ src/app/simple-counter1/simple-counter1.component.ts
@@ -13,9 +13,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 export class SimpleCounter1 {
   @Input() title: string;
   @Input() counterSimple1: number;
-  @Output() update1 = new EventEmitter<number>();
-  
+  /* 
+    -- OLD - BUT working! - versione for COUNTER1 
+    --
+  @Output() update = new EventEmitter<number>();
   onClick(event: MouseEvent) {
-    this.update1.emit(1);
+    this.update.emit(1);
+  */
+  
+  @Output() counterSimple1Change = new EventEmitter<number>();
+  onClick(event: MouseEvent) {
+    this.counterSimple1Change.emit(1);
   }
 }
