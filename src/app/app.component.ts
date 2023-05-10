81d543841fab3883136224e9777b07f957fd8e89
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-10 12:30:08 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  caricamento = 0;
+  avanzamento = 0;
 
   ngOnInit(): void {
     this.updateProgress(10);
   }
   updateProgress(value: number): void {
-    if (this.caricamento >= 100 || value === 0) {
+    if (this.avanzamento >= 100 || value === 0) {
       return;
     }
-    const delta = 100 - this.caricamento;
+    const delta = 100 - this.avanzamento;
     if (value > delta) {
       value = delta;
     }
     setTimeout(() => {
-      this.caricamento += value;
+      this.avanzamento += value;
       this.updateProgress(value);
     }, 1000);
   }
